dffad61e55a52d547895338921124e24d50424bf
Hamdi ELFEKIH <hamdi.elfekih@outlook.com>
2023-09-05 17:56:21 +0100

    Initial commit

---
+++ src/main/java/com/hamdiElfekih/SelfManagementSystem/SelfManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hamdiElfekih.SelfManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamdiElfekih/SelfManagementSystem/SelfManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamdiElfekih.SelfManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
