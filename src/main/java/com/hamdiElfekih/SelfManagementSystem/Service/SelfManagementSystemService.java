b0b057ce7771ffd3d0a26a962c3cc78345546dd0
Hamdi ELFEKIH <hamdi.elfekih@outlook.com>
2023-09-05 23:01:48 +0100

    Implement MCV architecture and connect to MySql server

---
+++ src/main/java/com/hamdiElfekih/SelfManagementSystem/Service/SelfManagementSystemService.java
@@ -0,0 +1,11 @@
+package com.hamdiElfekih.SelfManagementSystem.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SelfManagementSystemService {
+
+    public String getName(){
+        return "Test";
+    }
+}
