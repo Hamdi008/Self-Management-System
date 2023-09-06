6057d4f5dc93baa4dd365a4e40a45647365b5267
Hamdi ELFEKIH <hamdi.elfekih@outlook.com>
2023-09-06 14:52:17 +0100

    add spring-boot-starter-validation dependency

--- src/main/java/com/hamdiElfekih/SelfManagementSystem/Entity/User.java
+++ src/main/java/com/hamdiElfekih/SelfManagementSystem/Entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -15,6 +17,9 @@ public class User {
     private Long Id;
     private String firstName;
     private String lastName;
+
+    @Email(message = "Please enter a valid email Id", regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\\.[a-zA-Z.]{2,5}")
+    @NotNull(message = "Please enter a valid email Id")
     private String email;
     private String password;
     private String jobTitle;
